2269891e7a73369f6d0a66351fc9e9dced3f459a
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-07-01 21:40:53 +0530

    initial files

---
+++ src/main/java/com/cadt/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -0,0 +1,36 @@
+package com.cadt.rest.webservices.restful_web_services.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+
+  private static List<User> users = new ArrayList<>();
+
+  private static int usersCount = 3;
+
+  static {
+    users.add(new User(++usersCount, "mike", LocalDate.now().minusYears(30)));
+    users.add(new User(++usersCount, "eleven", LocalDate.now().minusYears(40)));
+    users.add(new User(++usersCount, "Will", LocalDate.now().minusYears(25)));
+  }
+
+  public List<User> findAll() {
+    return users;
+  }
+
+  public User save(User user) {
+    user.setId(++usersCount);
+    users.add(user);
+    return user;
+  }
+
+  public User findOne(int id) {
+    Predicate<? super User> predicate = user -> user.getId() == id;
+    return users.stream().filter(predicate).findFirst().get();
+  }
+}
