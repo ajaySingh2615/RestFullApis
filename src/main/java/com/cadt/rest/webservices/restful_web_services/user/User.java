92ca72d3aa8229d4ba947daf8b42f26321dee491
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-07-08 12:07:59 +0530

    "Part 1"

--- src/main/java/com/cadt/rest/webservices/restful_web_services/user/User.java
+++ src/main/java/com/cadt/rest/webservices/restful_web_services/user/User.java
@@ -1,49 +1,57 @@
 package com.cadt.rest.webservices.restful_web_services.user;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public class User {
 
-  private Integer id;
-  private String name;
-  private LocalDate birthDate;
+    private Integer id;
 
-  public User(Integer id, String name, LocalDate birthDate) {
-    this.id = id;
-    this.name = name;
-    this.birthDate = birthDate;
-  }
+    @Size(min = 2, message = "Name should have at least 2 characters")
+    private String name;
 
-  public User() {}
+    @Past(message = "Birth date should be in the past")
+    private LocalDate birthDate;
 
-  public Integer getId() {
-    return id;
-  }
+    public User(Integer id, String name, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+    public User() {
+    }
 
-  public String getName() {
-    return name;
-  }
+    public Integer getId() {
+        return id;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-  public LocalDate getBirthDate() {
-    return birthDate;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setBirthDate(LocalDate birthDate) {
-    this.birthDate = birthDate;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  @Override
-  public String toString() {
-    return (
-      "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]"
-    );
-  }
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return (
+                "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]"
+        );
+    }
 }
--- src/main/java/com/cadt/rest/webservices/restful_web_services/user/UserResource.java
+++ src/main/java/com/cadt/rest/webservices/restful_web_services/user/UserResource.java
@@ -2,7 +2,9 @@ package com.cadt.rest.webservices.restful_web_services.user;
 
 import java.net.URI;
 import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,32 +12,39 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import jakarta.validation.Valid;
+
 @RestController
 public class UserResource {
 
-  private UserDaoService service;
+    private UserDaoService service;
 
-  public UserResource(UserDaoService service) {
-    this.service = service;
-  }
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
 
-  @GetMapping("/users")
-  public List<User> retrieveAllUsers() {
-    return service.findAll();
-  }
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
 
-  @GetMapping("/users/{id}")
-  public User retrieveUser(@PathVariable int id) {
-    return service.findOne(id);
-  }
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        service.deleteById(id);
+    }
 
-  @PostMapping("/users")
-  public ResponseEntity<Object> createUser(@RequestBody User user) {
-    User savedUser = service.save(user);
-    URI Location = ServletUriComponentsBuilder.fromCurrentRequest()
-      .path("/{id}")
-      .buildAndExpand(savedUser.getId())
-      .toUri();
-    return ResponseEntity.created(Location).build();
-  }
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
+
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+
 }
